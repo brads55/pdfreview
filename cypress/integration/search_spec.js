aaa8f524cfa160d50ecb46d58094d02555d7ba6c
Daniel Bailey <daniel.bailey@arm.com>
2019-12-07 00:43:36 +0000

    More search tool stuff

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -25,7 +25,7 @@ describe('Search tool', ()=>{
 
     it('Finds matching texts on multiple pages', ()=>{
         cy.contains('Search for the words on this page.');
-        cy.get('body').type('{ctrl}f');
+        cy.get('div#button-search-toggle').click();
         cy.get('input#search-tool-query').should('be.visible').type('Search for the words{enter}');
         cy.get('div#sidebar-left-search-results').should('contain', '1:').and('contain', '2:');
         cy.get('div#sidebar-left-search-results').children().then(els => {
@@ -35,11 +35,15 @@ describe('Search tool', ()=>{
             cy.wrap(els[1]).click()
             cy.get('div#search-result-1').should('be.visible').should('have.class', 'selected');
         });
+        cy.get('div#button-search-toggle').click();
     });
 
     // This one currently fails because of a slight UX issue :( See issue #12
     // https://github.com/Franchie/pdfreview/issues/12
-    it.skip('Lets you jog up and down results using butons and keyboard shortfuts', ()=>{
+    it.skip('Lets you jog up and down results using butons and keyboard shortcuts', ()=>{
+        cy.contains('Search for the words on this page.');
+        cy.get('div#button-search-toggle').click();
+        cy.get('input#search-tool-query').should('be.visible').type('Search for the words{enter}');
         cy.get('div#sidebar-left-search-results').children().then(els => {
             cy.wrap(els).should('have.property', 'length', 2);
             cy.wrap(els[0]).click()
@@ -52,6 +56,7 @@ describe('Search tool', ()=>{
             cy.get('body').trigger('keydown', { keyCode: 114 })
                 .trigger('keyup', { keyCode: 114 })
             cy.get('div#search-result-1').should('be.visible').should('have.class', 'selected');
+            // Is this even how you do modifier keys????
             cy.get('body')
                 .trigger('keydown', { keyCode: 16 })
                 .trigger('keydown', { keyCode: 114 })
@@ -59,7 +64,50 @@ describe('Search tool', ()=>{
                 .trigger('keyup', { keyCode: 16 })
             cy.get('div#search-result-0').should('be.visible').should('have.class', 'selected');
         });
+        cy.get('div#button-search-toggle').click();
+    });
+
+    it('Can be case sensitive', ()=>{
+        cy.contains('Search for the words on this page.');
+        cy.get('div#button-search-toggle').click();
+        cy.get('input#search-tool-query').should('be.visible').type('search{enter}');
+        cy.get('div#sidebar-left-search-results').children().then(els => {
+            cy.wrap(els).should('have.property', 'length', 2);
+        });
+        cy.contains('Match case').click();
+        cy.get('div#sidebar-left-search-results').then(els =>{
+            var el = els[0];
+            cy.wrap(el.children).should('have.property', 'length', 0);
+        });
+        cy.contains('Match case').click();
+        cy.get('div#button-search-toggle').click();
+    });
+
+    it('Can use a regex', ()=>{
+        cy.contains('Search for the words on this page.');
+        cy.get('div#button-search-toggle').click();
+        cy.get('input#search-tool-query').should('be.visible').type('page (two|3){enter}');
+        cy.get('div#sidebar-left-search-results').then(els =>{
+            var el = els[0];
+            cy.wrap(el.children).should('have.property', 'length', 0);
+        });
+        cy.contains('Regex').click();
+        cy.get('div#sidebar-left-search-results').children().then(els => {
+            cy.wrap(els).should('have.property', 'length', 2);
+        });
+        cy.contains('Regex').click();
+        cy.get('div#button-search-toggle').click();
+    });
+
+    // https://github.com/Franchie/pdfreview/issues/16
+    it.skip('Can find multiple results on one line of text', ()=>{
+        cy.contains('Search for the words on this page.');
+        cy.get('div#button-search-toggle').click();
+        cy.get('input#search-tool-query').should('be.visible').type('a{enter}');
+        cy.get('div#sidebar-left-search-results').children().then(els => {
+            cy.wrap(els).should('have.property', 'length', 10);
+        });
+        cy.get('div#button-search-toggle').click();
     });
 });
-
 
